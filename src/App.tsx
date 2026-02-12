dbf9fa10e7ed97cd4d1517b271a56971565ec52c
Joohyeon28 <joohyeonstemmer28@gmail.com>
2026-02-12 19:52:45 +0200

    Fix: import correct Exception page (singular)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
-import AssetsList from './pages/AssetsList'
-import Dashboard from './pages/Dashboard' // you'll create this
+
+// Admin / Supervisor pages
+import Dashboard from './pages/Dashboard'
+import LiveSite from './pages/LiveSite'
+import SupervisorReview from './pages/SupervisorReview'
+import Workshop from './pages/Workshop'
+import Exception from './pages/Exception'
+
+// Controller pages
+import ControllerDashboard from './pages/ControllerDashboard'
+import ProductionInput from './pages/ProductionInput'
+import HourlyLogging from './pages/HourlyLogging'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+
+        {/* Admin / Supervisor routes */}
         <Route
-          path="/"
+          path="/dashboard"
           element={
             <ProtectedRoute>
               <Dashboard />
@@ -18,13 +30,66 @@ function App() {
           }
         />
         <Route
-          path="/assets"
+          path="/live-site"
           element={
             <ProtectedRoute>
-              <AssetsList />
+              <LiveSite />
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/supervisor-review"
+          element={
+            <ProtectedRoute>
+              <SupervisorReview />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/workshop"
+          element={
+            <ProtectedRoute>
+              <Workshop />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/exceptions"
+          element={
+            <ProtectedRoute>
+              <Exception />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Controller routes */}
+        <Route
+          path="/controller-dashboard"
+          element={
+            <ProtectedRoute>
+              <ControllerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/production-input"
+          element={
+            <ProtectedRoute>
+              <ProductionInput />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hourly-logging"
+          element={
+            <ProtectedRoute>
+              <HourlyLogging />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Default redirect – will be handled by ProtectedRoute based on role */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
   )
